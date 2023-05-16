192fb3754f651504436388bd9f9275360fc9c917
Mylène <m-b-c@mail.com>
2023-05-16 13:24:44 +0200

    removed unused folders

--- src/aspect_classification/models/evaluate.py
+++ src/aspect_classification/models/evaluate.py
@@ -1,13 +1,15 @@
 '''
 This is a script to use trained models to make predictions.
 '''
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
 import sys
 sys.path.append('/Users/mylene/BachelorsProject/Venue-Accessibility-Google-Reviews/src')
 from aspect_classification.data.data_cleaning import bert_processing
 from sklearn.preprocessing import LabelEncoder
 from pipelines import MyPipeline
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 import yaml
@@ -23,13 +25,14 @@ with open('src/aspect_classification/models/config.yml', 'r') as f:
     
 my_pipeline = MyPipeline(pipeline_type=params['pipeline_type'], bert_model=params['model_name'])
 
-def evaluate(y_true, y_pred):
-    accuracy = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred, average='weighted')
-    recall = recall_score(y_true, y_pred, average='weighted')
-    f1 = f1_score(y_true, y_pred, average='weighted')
-    confusion = np.array2string(confusion_matrix(y_true, y_pred))
-    return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1': f1, 'confusion_matrix': confusion}
+def evaluate(y_true, y_pred, labels):
+    # Check for missing labels
+    missing_labels = set(labels) - set(y_true)
+    if missing_labels:
+        raise ValueError(f"The following labels are missing from y_true: {missing_labels}")
+    report = classification_report(y_true, y_pred, labels=labels, output_dict=True)
+    confusion = confusion_matrix(y_true, y_pred, labels=labels)
+    return report, confusion
 
 def remove_rows(test_data):
     # Remove the redundant rows
@@ -57,11 +60,30 @@ def generate_results():
     # Convert y_true and y_pred to lists of strings
     y_true = [str(label) for label in y_true]
     y_pred = [str(label) for label in y_pred]
+    labels = ['Overview', 'Transport & Parking', 'Access', 'Toilets', 'Staff']
+    eval_metrics, confusion_graph = evaluate(y_true, y_pred, labels)
+    
+    # Convert y_pred to a pandas DataFrame
+    predicted_labels_df = pd.DataFrame({'Predicted Labels': y_pred})
 
-    eval_report = evaluate(y_true, y_pred)
+    # Save the predicted labels as a CSV file
+    predicted_labels_path = interim_path + "/predicted_labels.csv"
+    predicted_labels_df.to_csv(predicted_labels_path, index=False)
+    
+    # # Save the classification report as a text file
+    report_path = results_path+"_classification_report.tex"
+    report_df = pd.DataFrame(eval_metrics).transpose()
+    report_df.to_latex(report_path)
 
-    df = pd.DataFrame([eval_report])
-    df.to_csv(results_path, index=False)
+    print(f"Classification report saved as {results_path}")
+
+    # Visualize the confusion matrix
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(confusion_graph, annot=True, cmap='Blues', fmt='d', xticklabels=labels, yticklabels=labels)
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.savefig(results_path + '_confusion_matrix.png')
 
 if __name__ == '__main__':
     # Get the file paths from environment variables
@@ -69,7 +91,8 @@ if __name__ == '__main__':
     loaded_model_path = os.getenv('LOCAL_ENV') + 'models/aspect_classification/transformer_models/bert.joblib'
     # Define the directory path
     names = params['model_name'].split("/")
-    results_path = os.getenv('LOCAL_ENV') + f"results/aspect_classification/{names[1]}.csv"
+    results_path = os.getenv('LOCAL_ENV') + f"results/aspect_classification/{names[1]}"
+    interim_path = os.getenv('LOCAL_ENV') + 'data/interim'
 
     # Call the function to generate results
     generate_results()
