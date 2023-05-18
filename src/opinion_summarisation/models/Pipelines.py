1bc901ef87378af0a9bcca2fc3e6e13c519924b1
Mylène <m-b-c@mail.com>
2023-05-18 18:22:53 +0200

    edited the summarisation pipeline class

--- src/opinion_summarisation/models/Pipelines.py
+++ src/opinion_summarisation/models/Pipelines.py
@@ -1,28 +1,35 @@
 import yaml
 import torch
-from torch.utils.data import TensorDataset, DataLoader
-from summarizer import Summarizer, TransformerSummarizer
 import transformers
-from sklearn.pipeline import Pipeline
+from summarizer import Summarizer
+
+with open('src/opinion_summarisation/models/config.yml', 'r') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
 class SummarizationPipeline:
-    def __init__(self, config_file):
-        with open(config_file, 'r') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-        
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = transformers.AutoModelForSeq2SeqLM.from_pretrained(config['model_name'])
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(config['model_name'])
-        self.max_length = config['max_length']
-        self.num_beams = config['num_beams']
-        self.max_summary_length = config['max_summary_length']
-        
+    def __init__(self, model_type, model_name):
+        if model_type == 'seq2seq':
+            self.seq2seq_config = config['seq2seq_config']
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            self.model = transformers.AutoModelForSeq2SeqLM.from_pretrained(self.seq2seq_config['model_name'])
+            self.tokenizer = transformers.AutoTokenizer.from_pretrained(self.seq2seq_config['tokenizer_name'])
+            self.max_length = self.seq2seq_config['max_length']
+            self.num_beams = self.seq2seq_config['num_beams']
+            self.max_summary_length = self.seq2seq_config['max_summary_length']
+        else:
+            self.bert_config = config['berts']
+            if model_name == 'distilbert-base-uncased':
+                self.dist_config = self.bert_config['distilbert_config']
+                self.extractive_model = Summarizer(model_name, hidden=self.dist_config['hidden'], hidden_concat=self.dist_config['hidden_contact'])
+            else:
+                self.extractive_model = Summarizer(model_name)
+
     def __call__(self, text):
+        if self.extractive_model is not None:
+            summary = self.extractive_model(text)
+            return summary
+
         inputs = self.tokenizer(text, max_length=self.max_length, return_tensors='pt', truncation=True)
         summary_ids = self.model.generate(inputs['input_ids'], num_beams=self.num_beams, max_length=self.max_summary_length, early_stopping=True)
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return summary
-    
-    
-    
-    
--- src/opinion_summarisation/models/evaluate.py
+++ src/opinion_summarisation/models/evaluate.py
@@ -3,17 +3,23 @@ import torch
 import yaml
 import pandas as pd
 from rouge import Rouge
-from sklearn.metrics import classification_report
+import transformers
+from summarizer import Summarizer
 from Pipelines import SummarizationPipeline
 
 rouge = Rouge()
 
 with open('src/opinion_summarisation/models/config.yml', 'r') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
+# clean this up later
+se2seq_config = config['se2seq_config']
+bert_config = config['berts']['bert_config']
+distilbert_config = config['berts']['distilbert_config']
+sbert_config = config['berts']['sbert_config']
 
-pipeline = SummarizationPipeline(config['model_name'])
+pipeline = SummarizationPipeline('bert', bert_config['model_name'])
 
-def load_test_data(test_data_path : str):
+def load_test_data(test_data_path : str) -> torch.utils.data.TensorDataset:
     google_data = pd.read_csv(test_data_path)
     encodings = pipeline.tokenizer(google_data['Text'].tolist(), truncation=True, padding=True)
     google_tensors = torch.utils.data.TensorDataset(torch.tensor(encodings['input_ids']),
@@ -29,35 +35,48 @@ def compute_metrics(reference, predictions) -> dict:
     combined_pred = [' '.join(summary) for summary in predictions]
     scores = rouge.get_scores(combined_pred, combined_ref, avg=True)
     return scores
-    
 
-def evaluate_model(test_data_path : str, ref_data_path : str):
+def evaluate_model(test_data_path: str, ref_data_path: str):
     google_dataset = load_test_data(test_data_path)
-    data_loader = torch.utils.data.DataLoader(google_dataset, batch_size=config['batch_size'])
+    data_loader = torch.utils.data.DataLoader(google_dataset, batch_size=config['se2seq_config']['batch_size'])
     pipeline.model.eval()
     predictions = []
     with torch.no_grad():
         for batch in data_loader:
-            input_ids, attention_mask = batch
-            input_ids, attention_mask = input_ids.to(pipeline.device), attention_mask.to(pipeline.device)
-            outputs = pipeline.model.generate(input_ids=input_ids, attention_mask=attention_mask,
-                                              max_length=config['max_summary_length'], num_beams=config['num_beams'],
-                                              early_stopping=True)
-            summaries = pipeline.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            if isinstance(pipeline.extractive_model, Summarizer):
+                # BERT Extractive Summarizer model
+                input_ids = batch[0]
+                input_ids = input_ids.to(pipeline.device)
+                summaries = pipeline.extractive_model(input_ids)
+            elif isinstance(pipeline.model, transformers.BartForConditionalGeneration):
+                # Seq2Seq model
+                input_ids, attention_mask = batch
+                input_ids, attention_mask = input_ids.to(pipeline.device), attention_mask.to(pipeline.device)
+                outputs = pipeline.model.generate(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    max_length=pipeline.max_summary_length,
+                    num_beams=pipeline.num_beams,
+                    early_stopping=True
+                )
+                summaries = pipeline.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            else:
+                raise ValueError("Unsupported model type.")
+
             predictions.extend(summaries)
+
     reference_summaries = load_ref_data(ref_data_path)
     eval_metrics = pd.DataFrame(compute_metrics(reference_summaries, predictions))
-    eval_metrics.to_json(results_path)
+    eval_metrics.to_csv(results_path, index=False)
+
+
 
 
 if __name__ == '__main__':
     loaded_data_path = os.getenv('LOCAL_ENV') + 'data/interim/predicted_sentiment_labels.csv'
-    saved_model_path = os.getenv('LOCAL_ENV') + 'models/opinion_summarisation'
-    results_path = os.getenv('LOCAL_ENV') + 'results/opinion_summarisation/'+config['model_name']+'.csv'
+    saved_model_path = os.getenv('LOCAL_ENV') + 'models/opinion_summarisation/bertmodel.bin'
+    results_path = os.getenv('LOCAL_ENV') + 'results/opinion_summarisation/'+bert_config['model_name']+'.csv'
     
     pipeline.model = pipeline.model.from_pretrained(saved_model_path)
 
-    evaluate_model(loaded_data_path)
-
-
-    
+    evaluate_model(loaded_data_path, results_path)
