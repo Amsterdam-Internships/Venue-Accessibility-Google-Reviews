7b954f03e7d8cce3ca557d95fced5809edb1d421
Mylène <m-b-c@mail.com>
2023-10-30 19:22:26 +0100

    fixed path

--- src/sentiment_classification/models/train.py
+++ src/sentiment_classification/models/train.py
@@ -17,7 +17,7 @@ import yaml
 config_path = os.getenv('LOCAL_ENV') + '/src/sentiment_classification/models/config.yml'
 with open(config_path, 'r') as f:
     params = yaml.load(f, Loader=yaml.FullLoader)
-    # params = params['bert_params']
+    params = params['bert_params']
     
     
 my_pipeline = SentimentClassificationPipeline(pipeline_type='transformer', model_type=params['model_name_or_path'])
@@ -115,7 +115,7 @@ if __name__ == '__main__':
     loaded_data_path = os.getenv('LOCAL_ENV') + '/data/processed/aspect_classification_data/processed_euans_reviews.csv'
     saved_model_path = os.getenv('LOCAL_ENV') + 'models/sentiment_classification/transformer_models'
     logs_path = os.getenv('LOCAL_ENV') + '/logs/sentiment_classification/'
-    if params['bert_params'] == 'default':
+    if params == 'default':
         train_classic_models()
     else:
         train_bert_models()
