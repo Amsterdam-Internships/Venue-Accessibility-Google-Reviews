06ac991c95a548cdacab7bae98550cae4697dc29
Mylène <m-b-c@mail.com>
2023-12-12 14:50:37 +0100

    full dataset

--- src/sentiment_classification/models/train.py
+++ src/sentiment_classification/models/train.py
@@ -74,7 +74,7 @@ def train_bert_models():
     # load the data
     euans_data = pd.read_csv(loaded_data_path)
     # split the data 
-    train_dataset, val_dataset = create_datasets(euans_data[:4000])
+    train_dataset, val_dataset = create_datasets(euans_data)
     save_path = saved_model_path + f'/{names}'
     my_pipeline.training_args.output_dir = save_path
     data_collator = DataCollatorWithPadding(tokenizer=my_pipeline.tokenizer)
